c8292d27a69e4a9b67f2d9d31f1763c79b8d8c5a
Assiralaris <lpdacassiral@gmail.com>
2026-05-25 11:22:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ from telas.cadastro import tela_cadastro
 from telas.home import tela_home
 from telas.quiz_ao_vivo import tela_quiz_ao_vivo
 from telas.batalha_de_equipes import tela_batalha_de_equipes
-from telas.mini_provas.mini_provas import tela_mini_provas_mini_provas
 from telas.votacao import tela_votacao
 from telas.voto import tela_voto
 from telas.desafios import tela_desafios
+
+from telas.mini_provas.mini_provas import (
+    tela_mini_provas
+)
 
 st.set_page_config(
     page_title="Challenge System",
